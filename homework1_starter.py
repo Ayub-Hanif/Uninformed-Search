39420f73e57aef12a572e51f85d1f47672c42f59
Ayub <ayubhanif06@gmail.com>
2025-04-17 19:24:32 -0700

    Add initial implementation of homework assignment for ECS170, including N-Queens and Lights Out sections

---
+++ homework1_starter.py
@@ -0,0 +1,73 @@
+############################################################
+# ECS170: Uninformed Search
+############################################################
+
+student_name = "Type your full name here."
+
+############################################################
+# Imports
+############################################################
+
+# Include your imports here, if any are used.
+
+
+
+############################################################
+# Section 1: N-Queens
+############################################################
+
+def num_placements_all(n):
+    pass
+
+def num_placements_one_per_row(n):
+    pass
+
+def n_queens_valid(board):
+    pass
+
+def n_queens_solutions(n):
+    pass
+
+############################################################
+# Section 2: Lights Out
+############################################################
+
+class LightsOutPuzzle(object):
+
+    def __init__(self, board):
+        pass
+
+    def get_board(self):
+        pass
+
+    def perform_move(self, row, col):
+        pass
+
+    def scramble(self):
+        pass
+
+    def is_solved(self):
+        pass
+
+    def copy(self):
+        pass
+
+    def successors(self):
+        pass
+
+    def find_solution(self):
+        pass
+
+def create_puzzle(rows, cols):
+    pass
+
+############################################################
+# Section 3: Linear Disk Movement
+############################################################
+
+def solve_identical_disks(length, n):
+    pass
+
+def solve_distinct_disks(length, n):
+    pass
+
